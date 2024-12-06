1fd06c3d4f5981efcd430f5b8c7690edc4fec5e6
TitouMartin <titouanmartin@icloud.com>
2024-12-06 04:06:46 -0800

    basic landing page styling

--- pages/index.vue
+++ pages/index.vue
@@ -8,14 +8,34 @@
                     <div>a 30 years old</div>
                     <div>Paris based</div>
                 </div>
-                <div class="product-designer soap">
+                <div class="product-designer big-text soap">
                     <div>Product</div>
                     <div class="designer">Designer</div>
                 </div>
             </h1>
         </div>
-        <div class="text-block">I am a 7 years experienced <span class="outlined">Product Designer</span> that also dabbles in <span class="outlined">Product Management</span>.</div>
-        <div class="text-block about">I help teams to create and bring to life beautiful, intuitive and accessible digital products.</div>
-        <div class="text-block projects">Here are some projects I took a part in :</div>
+        <div class="text-block">
+            <div>I am a 7 years experienced <span class="outlined">Product Designer</span> that also dabbles in <span class="outlined">Product Management</span>.</div>
+            <div class="about">I help teams to create and bring to life beautiful, intuitive and accessible digital products.</div>
+            <div class="projects">Here are some projects I took a part in :</div>
+            
+            <div class="project-container">
+                <div class="project-image-container">
+                    <div class="project-background amikeo"></div>
+                     <div class="project-image amikeo"></div>
+                </div>
+                <div class="soap big-text amikeo">amikeo</div>
+            </div>
+
+            <div class="project-container sofya">
+                <div class="soap big-text sofya">sofya</div>
+                <div class="project-image-container">
+                    <div class="project-background sofya"></div>
+                     <div class="project-image sofya"></div>
+                </div>
+            </div>
+        </div>
+
+        <Contact />
     </div>
 </template>
