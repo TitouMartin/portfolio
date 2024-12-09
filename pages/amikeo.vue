d88a2dac68f228605d73650bebe66a9ffe28baf9
TitouMartin <titouanmartin@icloud.com>
2024-12-09 18:38:41 +0100

    started on amikeo page responsive

--- pages/amikeo.vue
+++ pages/amikeo.vue
@@ -9,53 +9,56 @@
             <div class="big-text amikeo">amikeo</div>
         </div>
 
-        <div class="text-block project-page-big-margin">
-            <div class="chips-container project-page-small-margin">
-                <div class="chip blue">Product Manager</div>
-                <div class="chip blue">Product Designer</div>
-                <div class="chip blue">Team Lead</div>
-                <div class="chip red">Accessibility</div>
-                <div class="chip red">Mobile & Desktop</div>
-                <div class="chip red">BtoC</div>
-                <div class="chip red">BtoB</div>
-            </div>
 
-            <div class="split project-page-small-margin">
-                <div class="block">
-                    <div class="section-title">About</div>
-                    <div><span class="outlined">Auticiel</span> specializes in supporting individuals with intellectual developmental disorders or autism spectrum disorders. <span class="outlined">Auticiel</span> offers the <span class="outlined">Amikeo Suite</span>, a set of 10 applications available to the general public, as well as a tablet equipped with custom software to ensure ease of use for all audiences. </div>
+        <div class="amikeo-el-order">
+            <div class="text-block project-page-small-margin order-text-1">
+                <div class="chips-container project-page-small-margin">
+                    <div class="chip blue">Product Manager</div>
+                    <div class="chip blue">Product Designer</div>
+                    <div class="chip blue">Team Lead</div>
+                    <div class="chip red">Accessibility</div>
+                    <div class="chip red">Mobile & Desktop</div>
+                    <div class="chip red">BtoC</div>
+                    <div class="chip red">BtoB</div>
                 </div>
-                <div class="block">
-                    <div class="section-title">Roles</div>
-                    <div>
-                        <ul>
-                            <li>Definition of objectives and key results for the product team and an annual roadmap aligned with them</li>
-                            <li>User reaserch and new features design</li>
-                            <li>Development progress monitoring, Q&A tests, sprint and roadmap advancement suppervision</li>
-                            <li>Visual standardization of the solution</li>
-                            <li>Mid-term product strategy definition in collaboration with the CEO</li>
-                        </ul>
+        
+                <div class="split">
+                    <div class="block">
+                        <div class="section-title">About</div>
+                        <div><span class="outlined">Auticiel</span> specializes in supporting individuals with intellectual developmental disorders or autism spectrum disorders. <span class="outlined">Auticiel</span> offers the <span class="outlined">Amikeo Suite</span>, a set of 10 applications available to the general public, as well as a tablet equipped with custom software to ensure ease of use for all audiences. </div>
+                    </div>
+                    <div class="block">
+                        <div class="section-title">Roles</div>
+                        <div>
+                            <ul>
+                                <li>Definition of objectives and key results for the product team and an annual roadmap aligned with them</li>
+                                <li>User reaserch and new features design</li>
+                                <li>Development progress monitoring, Q&A tests, sprint and roadmap advancement suppervision</li>
+                                <li>Visual standardization of the solution</li>
+                                <li>Mid-term product strategy definition in collaboration with the CEO</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div>
+            
+            <div class="text-block project-page-big-margin order-text-2">
                 <div class="section-title">Context</div>
                 <div>The position of Product Manager has been vacant for more than two years, and in the absence of a designer, all of <span class="outlined">Auticiel</span>'s products have been developed and maintained by two developers. This has resulted in a lack of coherence among the various implemented features, as well as limited visibility on specific actions required to evolve the product over the coming years.</div>
                 <div>Given that <span class="outlined">Auticiel</span>'s product team is small (4 developers and one product owner/designer) compared to the number of products (12 applications and two cloud platforms), the team’s backlog is very heavy. Therefore, it was necessary for me to identify the most critical areas for improvement and propose simple designs that could be quickly integrated.</div>
             </div>
-        </div>
-
-        <div class="project-big-image-container amikeo-10-iphones">
-            <img src="~/assets/images/amikeo/amikeo-10-iphones.png" />
-        </div>
-
-        <div class="project-image-double amikeo project-page-big-margin">
-            <div class="img-1-container">
-                <img src="~/assets/images/amikeo/amikeo-trial.png" />
+    
+            <div class="project-big-image-container amikeo-10-iphones order-img-1">
+                <img src="~/assets/images/amikeo/amikeo-10-iphones.png" />
             </div>
-            <div class="img-2-container">
-                <img src="~/assets/images/amikeo/amikeo-success.png" />
+    
+            <div class="project-image-double amikeo project-page-big-margin order-img-2">
+                <div class="img-1-container">
+                    <img src="~/assets/images/amikeo/amikeo-trial.png" />
+                </div>
+                <div class="img-2-container">
+                    <img src="~/assets/images/amikeo/amikeo-success.png" />
+                </div>
             </div>
         </div>
 
